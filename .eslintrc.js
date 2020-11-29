8d6d469b3bf1bcebd14c1c03f976a43a27829bbf
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-11-29 18:17:37 +0900

    chore: fix some settings and added plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,18 @@
 module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint'],
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
+    es6: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
-  rules: {},
 };
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import Express from 'express';
+import * as Express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 

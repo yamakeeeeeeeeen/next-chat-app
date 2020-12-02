b2bc88e237771650334c5a196660cf64e4d0ee05
yamakeeeeeeeeen <yamakeeeeeeeeen@gmail.com>
2020-12-03 03:07:55 +0900

    feat: create channels-event api

---
+++ src/pages/api/pusher/channels-event.ts
@@ -0,0 +1,35 @@
+import Pusher from 'pusher';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const {
+  NEXT_PUBLIC_APP_ID: appId,
+  NEXT_PUBLIC_KEY: key,
+  NEXT_PUBLIC_SECRET: secret,
+  NEXT_PUBLIC_CLUSTER: cluster,
+} = process.env;
+
+const pusher = new Pusher({
+  appId,
+  key,
+  secret,
+  cluster,
+});
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const data = req.body;
+
+  await new Promise<void>((resolve, reject) => {
+    pusher
+      .trigger('test-channel', 'event-name', data)
+      .then((response) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json('sent event successfully');
+        res.end(JSON.stringify(response));
+        resolve();
+      })
+      .catch((error) => {
+        console.error(error);
+        reject();
+      });
+  });
+};
